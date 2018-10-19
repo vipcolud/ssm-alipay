a0c9ee1cc63707ac8fb8f244eae7175465a8dfbc
e <1414200300@qq.com>
2018-10-19 10:18:09 +0800

    3

---
+++ src/main/java/com/mmall/dao/OrderItemMapper.java
@@ -0,0 +1,30 @@
+package com.mmall.dao;
+
+import com.mmall.pojo.OrderItem;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface OrderItemMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(OrderItem record);
+
+    int insertSelective(OrderItem record);
+
+    OrderItem selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(OrderItem record);
+
+    int updateByPrimaryKey(OrderItem record);
+
+    List<OrderItem> getByOrderNoUserId(@Param("orderNo")Long orderNo, @Param("userId")Integer userId);
+
+    List<OrderItem> getByOrderNo(@Param("orderNo")Long orderNo);
+
+
+
+    void batchInsert(@Param("orderItemList") List<OrderItem> orderItemList);
+
+
+}
